ca4a0ec068cc52c2f1223ebc2b466db0ea78f917
sarah4000 <00001095128309sp@al.educacao.sp.gov.br>
2024-09-04 09:22:34 -0300

    Update sKetch.js

--- sKetch.js
+++ sKetch.js
@@ -1,15 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.3/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.9.3/addons/p5.sound.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <meta charset="utf-8" />
+function setup() {
+  createCanvas(600, 600);
+}
 
-  </head>
-  <body>
-    <main>
-    </main>
-    <script src="sketch.js"></script>
-  </body>
-</html>
+function draw() {
+  background("black");
+  fill("white");
+  textSize(64);
+  textAlign(CENTER, CENTER)
+  
+  let maximo = width;
+  let minimo = 0;
+  let palavra = "ja posso ir embora daqui ?";
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+  let parcial = palavra.substring(0,quantidade);
+  text(parcial,200,200);
+  
+//  if(mouseX < 50){
+//    let palavra = "C";
+//    text(palavra, 200, 200);
+//  } else {
+//    let palavra = "Caminhante";
+//    text(palavra, 200, 200);
+//  }
+}
